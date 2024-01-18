bca25a9676fa4bbb3d97840c14bba8df0d110361
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-18 15:53:01 -0500

    add app bar

--- app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -4,36 +4,31 @@ import SuperheroesTheme
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.FloatingActionButtonDefaults.elevation
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -73,12 +68,29 @@ fun HeroCard(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SuperheroesTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    style = MaterialTheme.typography.displayLarge,
+                    text = "Superheroes",
+                )
+            }
+        })
+}
+
 @Composable
 fun HeroAvatar(
     modifier: Modifier = Modifier,
     @DrawableRes imageResId: Int,
 ) {
-    Box(modifier = modifier.clip(RoundedCornerShape(8.dp)).aspectRatio(1f)) {
+    Box(modifier = modifier
+        .clip(RoundedCornerShape(8.dp))
+        .aspectRatio(1f)) {
         Image(
             painter = painterResource(id = imageResId),
             contentDescription = null,
@@ -110,10 +122,19 @@ fun HeroesList(
     modifier: Modifier = Modifier,
     heroes: List<Hero>,
 ) {
-    LazyColumn(
-        modifier = modifier.padding(8.dp)) {
-        items(heroes) {
-            HeroCard(hero = it, modifier = Modifier.padding(8.dp))
+    Scaffold(
+        topBar = {
+            SuperheroesTopAppBar()
+        }
+    ) {
+
+        LazyColumn(
+            contentPadding = it,
+            modifier = modifier.padding(8.dp)
+        ) {
+            items(heroes) {hero ->
+                HeroCard(hero = hero, modifier = Modifier.padding(8.dp))
+            }
         }
     }
 }
--- app/src/main/java/com/example/superheroes/MainActivity.kt
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -5,13 +5,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.superheroes.model.HeroesRepository
 
 class MainActivity : ComponentActivity() {
