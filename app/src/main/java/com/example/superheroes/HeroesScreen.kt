1541107ec7340ddb137c4e6c79c980d949b3e8fa
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-01-18 15:42:55 -0500

    clean up styling to match requirements

--- app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -5,15 +5,18 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -29,6 +32,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -47,17 +51,24 @@ fun HeroCard(
             defaultElevation = 2.dp
         ),
         modifier = modifier
-            .background(MaterialTheme.colorScheme.primaryContainer)
-            .clip(
-                RoundedCornerShape(16.dp)
-            )
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(16.dp)
     ) {
-        Row(modifier = modifier
-            .padding(16.dp)
-            .height(72.dp)) {
-            HeroDetails(nameResId = hero.nameResId, descriptionResId = hero.descriptionResId)
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .height(72.dp),
+        ) {
+            HeroDetails(
+                modifier = Modifier.weight(1f),
+                nameResId = hero.nameResId, descriptionResId = hero.descriptionResId)
             Spacer(modifier = Modifier.width(16.dp))
-            HeroAvatar(imageResId = hero.imageResId)
+            HeroAvatar(
+                imageResId = hero.imageResId,
+                modifier = Modifier.requiredSize(72.dp),
+            )
         }
     }
 }
@@ -67,11 +78,10 @@ fun HeroAvatar(
     modifier: Modifier = Modifier,
     @DrawableRes imageResId: Int,
 ) {
-    Box(modifier = modifier.size(72.dp).clip(RoundedCornerShape(8.dp)).aspectRatio(1f)) {
+    Box(modifier = modifier.clip(RoundedCornerShape(8.dp)).aspectRatio(1f)) {
         Image(
             painter = painterResource(id = imageResId),
             contentDescription = null,
-            modifier = modifier
         )
     }
 }
@@ -80,8 +90,9 @@ fun HeroAvatar(
 fun HeroDetails(
     @StringRes nameResId: Int,
     @StringRes descriptionResId: Int,
+    modifier: Modifier = Modifier,
 ) {
-    Column {
+    Column(modifier = modifier) {
         Text(
             text = stringResource(id = nameResId),
             style = MaterialTheme.typography.displaySmall,
